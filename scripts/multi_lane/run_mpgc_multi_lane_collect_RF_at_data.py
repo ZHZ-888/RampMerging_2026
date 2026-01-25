fa1870de19622c470080166981a6afe45cce1a24
ZHZ-888 <2412674271@qq.com>
2026-01-25 18:31:18 +1300

    only gen ramp veh

--- scripts/multi_lane/run_mpgc_multi_lane_collect_RF_at_data.py
+++ scripts/multi_lane/run_mpgc_multi_lane_collect_RF_at_data.py
@@ -92,9 +92,9 @@ def loop(traci, st, data_recorder, veh_gen, formation_controller, merging_contro
         traci.simulationStep()  # start simulation
         # mainlane vehicle generation
         # veh_gen.veh_gen_homo(step, m1_dpt_type, 'm', 'route0', 27.5, '1')  # 30m/s => 110km/h
-        veh_gen.veh_gen_homo(step, m1_dpt_type, 'm', 'route0', 27.5, '0')  # 30m/s => 110km/h
+        # veh_gen.veh_gen_homo(step, m1_dpt_type, 'm', 'route0', 27.5, '0')  # 30m/s => 110km/h
         # ramp vehicle generation
-        # veh_gen.veh_gen_heter2(step, r_dpt_type, 'r', r_autoFollow_p)
+        veh_gen.veh_gen_heter2(step, r_dpt_type, 'r', r_autoFollow_p)
 
         # disable lane-changing
         data_recorder.disable_all_lane_changes()
@@ -169,7 +169,7 @@ if __name__ == '__main__':
         r_autoFollow_p = 1,
         r_platoon_p = 1,
         loss_rate = 0,
-        gui = False,
+        gui = True,
         plot = False,
         display = False,
         lc = False,
@@ -179,5 +179,5 @@ if __name__ == '__main__':
 
     # orgnise data
     output_path = "/home/zzha/PycharmProjects/RampMerging_2026/data/features/df_rf_at_260123.csv"
-    collect_RF_data(dic_targets, ls_features, output_path)
+    # collect_RF_data(dic_targets, ls_features, output_path)
 
