1b4571f462447d09f82380d5ee378403a23b30c0
ZHZ-888 <2412674271@qq.com>
2026-08-19 21:06:49 +1200

    density-based rule switch

--- functions/merging_controller.py
+++ functions/merging_controller.py
@@ -94,14 +94,14 @@ class MergingController:
         #     ls_r_leader_up
         # )
 
-        # regular_mode, jam_mode = self.mode_switch.determine_mode_flexible_merge_point(
-        #     ls_wsB_asc,
-        #     ls_wsA_asc,
-        #     ls_r_leader_up)
+        regular_mode, jam_mode = self.mode_switch.determine_mode_flexible_merge_point(
+            ls_wsB_asc,
+            ls_wsA_asc,
+            ls_r_leader_up)
 
-        regular_mode, jam_mode = self.mode_switch.determine_mode_low_sensor_reliance(
-            ls_r_leader_wsA_asc,
-            ls_wsB_av_asc)
+        # regular_mode, jam_mode = self.mode_switch.determine_mode_low_sensor_reliance(
+        #     ls_r_leader_wsA_asc,
+        #     ls_wsB_av_asc)
 
         # jam_mode = True
         # regular_mode = False
